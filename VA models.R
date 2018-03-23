41cf16b66c0c6bce1fda3b8bf9bbc430676df7dd
Taiki Wada <tykiwada@Taikis-MacBook-Pro.local>
2018-03-23 14:36:25 -0600

    path

---
+++ VA models.R
@@ -0,0 +1,35 @@
+# hypergeometric functions
+
+# M,N,n
+
+# As a first-time home buyer, being aware of certain types of loans is very important. 
+# There are several types of loans offered
+
+# Conventional loans
+
+# FHA loans
+
+# VA guaranteed loans
+
+# FSA/RHS-guaranteed FSA/RHS(Farm Service Agency or Rural Housing Service
+
+# https://www.nolo.com/legal-encyclopedia/what-the-difference-between-conventional-fha-va-loan.html
+
+            
+
+# Race and application denial. Is there profiling involved?
+
+# Ho: Race does not explain denial of application.
+
+# Each factor level of race needs to have a frequency table and histogram.
+
+# Logistic regression run.
+
+# Phylogenetic tree run to see if there are possible correlations.
+
+
+
+
+
+
+
