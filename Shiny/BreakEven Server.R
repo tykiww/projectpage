d1d60beea4b5536b03f003def538fb7fd14a1c54
Taiki Wada <tykiwada@Taikis-MacBook-Pro.local>
2018-08-12 00:19:20 +0900

    yes let's do this midnight coding

---
+++ Shiny/BreakEven Server.R
@@ -0,0 +1,43 @@
+library(shiny)
+library(FinCal)
+library(plotly)
+library(tidyverse)
+
+# server
+server <- function(input, output) {
+  # Objects
+  TC <- reactive(round((input$variableCost * input$UnitSale) + (input$FixedCost + (input$investment / input$life)), 0)) 
+  TR <- reactive(round(input$UnitSale * input$UnitPrice, 0)) 
+  profit <- reactive(round(input$UnitSale*input$UnitPrice - (input$variableCost*input$UnitSale +(input$FixedCost+(input$investment/input$life))), 0))
+  CBE <- reactive(round(input$FixedCost/(input$UnitPrice-input$variableCost), 2) %>% 
+                    paste(c("units"), sep = " ")) 
+  # convert to output
+  output$TC <- renderText(TC())
+  output$TR <- renderText(TR())
+  output$profit <- renderText(profit())
+  output$CBE <- renderText(CBE())
+  
+  # Render Plot
+  output$Plot <- renderPlotly({
+    unitsX <- round(seq(0, input$UnitSale*3, by=(input$UnitSale*input$life)/30), 0)
+    TRevenue = NULL
+    for (i in 1:length(unitsX)) {
+      TRevenue[i] = unitsX[i]*input$UnitPrice
+      TRevenue
+    }
+    TCost = NULL
+    for (i in 1:length(unitsX)) {
+      TCost[i] = (unitsX[i]*input$variableCost)+(input$FixedCost+(input$investment/input$life))
+      TCost
+    }
+    FCost <- input$FixedCost+(input$investment/input$life)
+    Legend <- list(font = list(size=9), x=.1, y=1)
+    # output Plotly
+    plot_ly(x=unitsX, y=((TRevenue+TCost)/1000)) %>%
+      layout(yaxis = list(title = 'Revnue (in 1000s)'), xaxis = list(title = 'Units Sold'), legend = Legend) %>%
+      add_trace(y=TRevenue, line = list(color = 'black'), name = 'Revenue', mode = 'lines') %>%
+      add_trace(y=FCost, line = list(color = 'red'), name = 'Fixed costs', mode = 'lines') %>%
+      add_trace(y=TCost, line = list(color = 'blue'), name = 'Total costs', mode = 'lines')
+  })
+}
+
---
+++ Shiny/BreakEven UI.R
@@ -0,0 +1,42 @@
+# UI set up
+{
+  # import fonts
+  fontImp <- tags$style(HTML("@import url('https://fonts.googleapis.com/css?family=PT+Sans');"))
+  # font 1
+  font1 <- "font-family: 'PT Sans', sans-serif; color: #85144b; font-size: 35px;"
+  # font 2
+  font2 <- "font-family: 'PT Sans', sans-serif; color: #85144b; font-size: 15px;"
+  # font 3
+  font3 <- "font-family: 'PT Sans', sans-serif; color: #85144b; font-size: 14px;"
+  # bold
+  bold <- "font-weight: bold;"
+  
+  # Title Panel
+  mainTitle <- titlePanel(h1("Break-even",style = font1))
+  
+  # Top Body
+  col1 <- column(4, style = font2,
+                 wellPanel(numericInput("UnitSale", "Expected units sold per year", value = 0, min = 0),
+                           numericInput("UnitPrice", "Expected price per unit", value = 0, min = 0)))
+  col2 <- column(4, style = font2,
+                 wellPanel(numericInput("variableCost", "Variable cost per unit",value = 0, min = 0),
+                           numericInput("FixedCost", "Total Fixed cost", value = 0, min = 0)))
+  col3 <- column(4, style = font2,
+                 wellPanel(numericInput("investment", "Initial investment", value = 0, min = 0),
+                           numericInput("life", "Investment (yrs)",value = 0, min = 0)))
+  
+  topBody <- fluidRow(col1 , col2, col3)
+  
+  # Bottom Body
+  bottomLeft <- column(3, style = font3,
+                       wellPanel(
+                         h5("Total revenue"), textOutput("TR"),
+                         h5("Total cost"), textOutput("TC"),
+                         h5("Profit"), textOutput("profit"),
+                         h5("Break-even Volume"), textOutput("CBE")))
+  
+  bigPlot <- column(9, wellPanel(style="background-color: maroon;", plotlyOutput("Plot")))
+} 
+
+# user interface
+ui <- fluidPage( fontImp, mainTitle, topBody,bigPlot, bottomLeft)
